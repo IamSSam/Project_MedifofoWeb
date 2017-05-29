55d5294f2988396b9e95a452cb6671f05f03bdcd
IamSSam <kongse92@gmail.com>
2017-05-29 22:36:06 +0900

    Add Login page

---
+++ Medifofo_web/doctor_register.js
@@ -0,0 +1,32 @@
+function registerDoctor(){
+  $('registerButton').on("click", function(){
+    var email = $('email').val();
+    var password = $('password').val();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    });
+
+  });
+}
+
+$(function() {
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
