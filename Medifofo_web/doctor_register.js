31ba69e326237c666d16031af9df00fbda667b63
IamSSam <kongse92@gmail.com>
2017-05-31 12:19:30 +0900

    Modify firebase init(important)

--- Medifofo_web/doctor_register.js
+++ Medifofo_web/doctor_register.js
@@ -1,32 +1,51 @@
-function registerDoctor(){
-  $('registerButton').on("click", function(){
-    var email = $('email').val();
-    var password = $('password').val();
+window.onload = function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDiXW9MnTR5ME_brg38JUdUe808V6o0mL0",
+    authDomain: "medifofoweb.firebaseapp.com",
+    databaseURL: "https://medifofoweb.firebaseio.com",
+    projectId: "medifofoweb",
+    storageBucket: "medifofoweb.appspot.com",
+    messagingSenderId: "744363956828"
+  };
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  firebase.initializeApp(config);
+};
+
+function registerDoctor(){
+  const txtEmail = document.getElementById('email');
+  const txtPassword = document.getElementById('password');
+  const buttonRegister = document.getElementById('registerButton');
+
+  buttonRegister.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const auth = firebase.auth();
+
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 8) {
+      alert('Please enter a password.');
+      return;
+    }
+
+    auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ...
+
+    console.log(errorMessage);
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser){
+        console.log(firebaseUser);
+      }else{
+        console.log('not logged in');
+      }
     });
 
+    });
   });
-}
-
-$(function() {
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
+};
