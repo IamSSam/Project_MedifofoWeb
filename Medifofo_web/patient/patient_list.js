c246fca30ea8bac82732deaef3ac31d2392da45c
IamSSam <kongse92@gmail.com>
2017-06-07 23:58:54 +0900

    Connect dynamic table and mireene database

--- Medifofo_web/patient/patient_list.js
+++ Medifofo_web/patient/patient_list.js
@@ -1,5 +1,4 @@
 $(function() {
-
   firebase.auth().onAuthStateChanged(user => {
     if(user) {
       console.log(user);
@@ -19,6 +18,72 @@ $(function() {
       console.log('not logged in');
     }
   });
+
+
+  var table = $('#datatable-buttons2').DataTable( {
+    searching: true,
+    paging: true,
+    dom: "Bfrtip",
+    buttons: [
+    {
+      extend: "copy",
+      className: "btn-sm"
+    },
+    {
+      extend: "csv",
+      className: "btn-sm"
+    },
+    {
+      extend: "excel",
+      className: "btn-sm"
+    },
+    {
+      extend: "pdfHtml5",
+      className: "btn-sm"
+    },
+    {
+      extend: "print",
+      className: "btn-sm"
+    },
+    ],
+    "language": {
+            "lengthMenu": "Display _MENU_ records per page",
+            "zeroRecords": "Nothing found - sorry",
+            "info": "Showing page _PAGE_ of _PAGES_",
+            "infoEmpty": "No records available",
+            "infoFiltered": "(filtered from _MAX_ total records)"
+        }
+  } );
+
+  var counter = "";
+
+  $.ajax({
+    url: 'http://igrus.mireene.com/medifofo_web/php/patient_list_queue.php',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data){
+      console.log(data[10]);
+      var patientInformationText = data[10].information_text;
+      var patient = patientInformationText.split(", ");
+      var reservedDate = data[10].datetime.substring(0,10);
+
+      table.row.add( [
+          counter + patient[8],
+          counter + patient[9],
+          counter + patient[10],
+          counter + patient[11],
+          counter + reservedDate,
+          counter + ""
+      ] ).draw( false );
+
+      counter++;
+
+    },
+    error: function(request, status, error){
+      console.log(request, status, error);
+    },
+  });
+
 });
 
 $(document).ready(function(){
