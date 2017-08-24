ff4443f46d3cf8aa50f3f6667670aab19c156300
IamSSam <kongse92@gmail.com>
2017-08-25 01:11:23 +0900

    Add datatable's data

--- Medifofo_web/main/doctor_main.js
+++ Medifofo_web/main/doctor_main.js
@@ -1,6 +1,23 @@
 var table;
 
-$(function() {
+$(document).ready(function(){
+  var scroll_start = 0;
+  var startchange = $('#main-content');
+  var offset = startchange.offset();
+  $(document).scroll(function() {
+     scroll_start = $(this).scrollTop();
+     if(scroll_start > offset.top) { // 스크롤 내려갔을 때
+         $('#navbar').css('background-color', '#00d2a5');
+         $('#home').css('color', '#fff');
+         $('#fh5co-logo').css('color', '#fff');
+         $('#nav-user-name').css('color', '#fff');
+      } else {
+         $('#navbar').css('background-color', '#ffffff');
+         $('#home').css('color', '#00d2a5');
+         $('#fh5co-logo').css('color', '#73879C');
+         $('#nav-user-name').css('color', '#000');
+      }
+  });
 
   firebase.auth().onAuthStateChanged(user => {
     if(user) {
@@ -9,10 +26,13 @@ $(function() {
       var user = firebase.auth().currentUser;
       var user_name = user.email;
       var user_name = user.displayName;
+      var user_picture = "http://graph.facebook.com/" + user.providerData[0].uid +"/picture?type=small";
       console.log(user);
       console.log(user_name);
       //document.getElementById('user-name').innerHTML = user_name;
+      document.getElementById('nav-user-image').src = user_picture;
       document.getElementById('nav-user-name').innerHTML = user_name;
+
 
     }else{
       console.log('not logged in');
@@ -33,10 +53,9 @@ $(function() {
     searching: true,
     paging: true,
     dom: '<"top"f>rt<"bottom"ip><"clear">',
-    
-});
 
-$(document).ready(function(){
+  });
+
   $("#logoutButton").click(function(){
     firebase.auth().signOut().then(function() {
     // Sign-out successful.
@@ -56,4 +75,59 @@ $(document).ready(function(){
       console.log(error);
     });
   });
+
+  var counter = "";
+
+  $.ajax({
+    url: 'http://igrus.mireene.com/medifofo_web/php/patient_list_queue.php',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data){
+      for(var i = 0; i < data.length; i++){
+        console.log(data[data.length-1]);
+      }
+      var patientInformationText = data[data.length-1].information_text;
+      var patientQuestionText = data[data.length-1].question_text;
+      var patient = patientInformationText.split(", ");
+      var reservedDate = data[data.length-1].datetime.substring(0,10);
+
+      table.row.add( [
+          counter + patient[8],
+          counter + patient[9],
+          counter + patient[10],
+          counter + patient[11],
+          counter + reservedDate,
+          counter + "열람"
+      ] ).draw( false );
+
+      counter++;
+
+      $('#datatable-buttons2').on('click', 'tr', function () {
+        var table_data = table.row( this ).data();
+        //alert( 'You clicked on '+data[0]+'\'s row' );
+        var mymodal = $('#myModal');
+        mymodal.find('.modal-name').text(table_data[0]);
+        mymodal.find('.modal-birth').text(table_data[3]);
+        mymodal.find('.modal-gender').text(table_data[2]);
+        mymodal.find('.modal-height').text(patient[0]);
+        mymodal.find('.modal-weight').text(patient[1]);
+        mymodal.find('.modal-abo').text(patient[2]);
+        mymodal.find('.modal-medicine').text(patient[3]);
+        mymodal.find('.modal-allery').text(patient[4]);
+        mymodal.find('.modal-history').text(patient[5]);
+        mymodal.find('.modal-sleeptime').text(patient[6]+" hours");
+        mymodal.find('.modal-dailystride').text(patient[7]+" steps");
+        mymodal.find('.modal-symptom').text(patientQuestionText);
+        mymodal.find('.modal-comment').text(patient[12]);
+        mymodal.modal('show');
+      });
+
+
+    },
+    error: function(request, status, error){
+      console.log(request, status, error);
+    },
+  });
+
+
 });
